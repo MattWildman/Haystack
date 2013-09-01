777f3cbf851b6301da4599620e4a471fc7e207b4
Matt Wildman <matthewwildman@gmail.com>
2013-09-01 19:23:22 +0100

    finished date-time selection

--- src/main/webapp/resources/js/date-time.js
+++ src/main/webapp/resources/js/date-time.js
@@ -1,10 +1,33 @@
 var useRange = false;
 
+//build and set full date time strings on submit
 function setDateTime() {
-	
+	var eDate, lDate, eTime, lTime;
+	if (!useRange) {
+		eDate = lDate = $('#only-date').val();
+		eTime = $('#earliest-time').val();
+		lTime = $('#latest-time').val();
+	}
+	else {
+		eDate = $('#earliest-date').val();
+		lDate = $('#latest-date').val();
+		eTime = $('#range-earliest-time').val();
+		lTime = $('#range-latest-time').val();
+	}
+	$('#earliest').val(eDate + " " + eTime);
+	$('#latest').val(lDate + " " + lTime);
 }
 
-//toggle single date and date range fieldsets
+//set up datepickers and placeholders
+$(function() {
+    var $dateInputs = $("#only-date, #earliest-date, #latest-date");
+    $dateInputs.datepicker({ maxDate: "+0d",
+    						 dateFormat: 'dd/mm/yy',
+    						 changeYear: true });
+    $dateInputs.attr('placeholder','dd/mm/yyyy');
+  });
+
+//toggle single date and date range usage
 $('input[name="use-range"').change(function() {
 	var $this = $(this);
 	if ($this.val() == 'yes') {
